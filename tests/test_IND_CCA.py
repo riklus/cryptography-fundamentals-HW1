182ff364e2222033eb03f746aed2e8fdacbb4d78
Riccardo Lussana <lussana.riccardo@gmail.com>
2025-09-28 16:47:57 +0200

    implemented CCA

--- tests/test_IND_CCA.py
+++ tests/test_IND_CCA.py
@@ -1,4 +1,7 @@
+import secrets
+from pytest import mark
 from myaes import MYAES
+
 
 def test_malleability():
     myaes = MYAES()
@@ -7,10 +10,29 @@ def test_malleability():
     enc = bytearray(myaes.encrypt(msg, key))
 
     # Attack: Attacker knows the message's format
-    pos = msg.find(b"1") + 16       # Find 100 DKK byte position, skipping the IV
-    enc[pos] ^= 0x04                # Flip 100 DKK to 500 DKK
+    pos = msg.find(b"1") + 16  # Find 100 DKK byte position, skipping the IV
+    enc[pos] ^= 0x04  # Flip 100 DKK to 500 DKK
 
     # Victim decrypts the message
     dec = myaes.decrypt(bytes(enc), key)
 
     assert dec == b"Transfer 500 DKK to Starbucks LT"
+
+
+@mark.parametrize("_", range(0, 1024))
+def test_IND_CPA_a(_):
+    myaes = MYAES()
+    key = myaes.keygen()
+
+    # Attacker can't access m1, m2, msg variable
+    m1, m2 = (b"\x00" * 16 + b"\x00" * 16), (b"\xff" * 16 + b"\xff" * 16)
+    msg = secrets.choice([m1, m2])
+    enc = myaes.encrypt(msg, key)
+
+    # Attacker drops last block from msg
+    dec = myaes.decrypt(enc[:-16], key)
+
+    if dec == b"\x00" * 16:
+        assert msg == m1
+    elif dec == b"\xff" * 16:
+        assert msg == m2
